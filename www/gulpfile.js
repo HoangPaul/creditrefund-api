3038edf6d0dcbdbfb4a3e5133efcb5a91f3ad2db
Paul Hoang <PaulKhoaHoang@gmail.com>
2016-05-02 22:25:21 +1000

    Ensure directory exists when compiling with gulp

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var mkdirp = require('mkdirp');
 
 gulp.task('default', function() {
     console.log('Hello world!');
 });
 
 gulp.task('compile', function() {
+    mkdirp('public/assets/css/');
+
     gulp.src('resources/assets/local/sass/styles.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename('styles.css'))
