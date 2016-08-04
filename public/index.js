e9c33effa321bc02d07bcbde963b02a1b0bc4f78
Paul Hoang <paulkhoahoang@gmail.com>
2016-08-05 08:11:41 +1000

    Update entrypoint for app
    Previously, the entrypoint was using the old configuration.

--- public/index.js
+++ public/index.js
@@ -1,19 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var api = require('app/api');
+var context = require('app/context');
+
 app.disable('x-powered-by');
 
-var api = require('app/api');
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send(JSON.stringify({
+        'latest': context.meta.version
+    }));
 });
-app.use(bodyParser.json());
-app.use('/api', api);
+
+// API
+app.use('/' + context.meta.version, function(req, res, next) {
+    req.context = context;
+    next();
+});
+app.use('/' + context.meta.version, api);
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log(JSON.stringify(context.meta, null, 2));
+    console.log('Credit Refund API started on http://%s:%s', host, port);
 });
+
